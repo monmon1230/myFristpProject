55e03874b62e1dc91a778246cb3bfef88e1ee41d
Mina Magdy <minamagdy57264@gmail.com>
2023-03-06 16:38:13 +0200

    initisting project, learning git

---
+++ script.js
@@ -0,0 +1,8 @@
+
+    import c from 'http';
+
+    const a= script2.largeNumber;
+    const b =6;
+
+    console.log(a+b);
+
---
+++ server.js
@@ -0,0 +1,15 @@
+import  express, { request, response }  from "express";
+import bodyParser from "body-parser";
+
+const app = express();
+app.use(express.static(__dirname + '/public'))
+
+
+
+
+
+
+
+
+
+app.listen(3000);
